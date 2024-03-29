f8966e72a963f1ff82cc440b6abd068372606031
kaviyarasan Ganesamoorthy <108562314+Kaviyarsan2004@users.noreply.github.com>
2024-03-29 07:56:48 -0400

    Update brommecountyny.py

--- brommecountyny.py
+++ brommecountyny.py
@@ -9,8 +9,8 @@ url = "https://gis.broomecountyny.gov/arcgis/rest/services/parcels/br_parcels/Ma
 unique_ids = set()
 
 # Define the range
-start_range_x = 200  # Start of the range (inclusive)
-end_range_x = 300    # End of the range (exclusive)
+start_range_x = 100  # Start of the range (inclusive)
+end_range_x = 200    # End of the range (exclusive)
 
 # Generate a random number within the range
 random_number_x = random.uniform(start_range_x, end_range_x)
@@ -29,11 +29,11 @@ with open('extracted_data_summa.csv', mode='r') as csv_file:
         unique_ids.add(row['ID'])
 
 # Open CSV file in append mode
-with open('extracted_data_summa.csv', mode='a', newline='') as csv_file:
-    fieldnames = ['ID', 'NAME', 'ADDRESS', 'LANDUSE']
+with open('extracted_data_summa_1.csv', mode='a', newline='') as csv_file:
+    fieldnames = ['FID', 'ID', 'NAME', 'ADDRESS', 'ZIP', 'MUNI', 'MAIL_ADDR', 'MAIL_CITY', 'LANDUSE', 'LAT', 'LONG', 'SWISID', 'TAXMAP', 'LOCATION']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     
-    # # Write CSV header
+    # Write CSV header
     # writer.writeheader()
 
     # Define the step size for the grid
@@ -41,10 +41,9 @@ with open('extracted_data_summa.csv', mode='a', newline='') as csv_file:
     step_y=random_number_y
 
 
-    # Define the initial x value
-    x =996300.0607814911
-    y=732337.0410720478
-
+    # Define the initial x & y value
+    x =1062310.6985232728
+    y=767545.505026855
     # Increment x until it reaches the maximum value
     while x <=  1151663.622417932:
         image_display = "1532,281,96"
@@ -74,12 +73,6 @@ with open('extracted_data_summa.csv', mode='a', newline='') as csv_file:
             extracted_data = []
             for result in data.get("results", []):
                 attributes = result.get("attributes", {})
-                extracted_data.append({
-                    "ID": attributes.get("ID"),
-                    "NAME": attributes.get("NAME"),
-                    "ADDRESS": attributes.get("ADDRESS"),
-                    "LANDUSE": attributes.get("LANDUSE")
-                })
                 ID = attributes.get("ID")
                 if ID not in unique_ids:
                     print("ID:", ID)
@@ -88,10 +81,20 @@ with open('extracted_data_summa.csv', mode='a', newline='') as csv_file:
                     print("Landuse:", attributes.get("LANDUSE"))
                     print()  # Add a blank line for clarity
                     writer.writerow({
-                        'ID': ID,
+                        'FID': attributes.get("FID"),
+                        'ID': attributes.get("ID"),
                         'NAME': attributes.get("NAME"),
                         'ADDRESS': attributes.get("ADDRESS"),
-                        'LANDUSE': attributes.get("LANDUSE")
+                        'ZIP': attributes.get("ZIP"),
+                        'MUNI': attributes.get("MUNI"),
+                        'MAIL_ADDR': attributes.get("MAIL_ADDR"),
+                        'MAIL_CITY': attributes.get("MAIL_CITY"),
+                        'LANDUSE': attributes.get("LANDUSE"),
+                        'LAT': attributes.get("LAT"),
+                        'LONG': attributes.get("LONG"),
+                        'SWISID': attributes.get("SWISID"),
+                        'TAXMAP': attributes.get("TAXMAP"),
+                        'LOCATION': attributes.get("LOCATION"),
                     })
                     unique_ids.add(ID)
             print(x)
@@ -103,7 +106,7 @@ with open('extracted_data_summa.csv', mode='a', newline='') as csv_file:
         # Increment x by step
         x += step_x
         csv_file.flush()
-        if x > 1151663.622417932:
+        if x > 1137023.3364511523:
             x=950095.4897688426
             y+=step_y
         if y>801669.4572406365:
