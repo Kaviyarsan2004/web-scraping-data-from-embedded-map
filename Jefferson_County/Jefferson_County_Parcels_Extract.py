741897e0ccaa1dc116c881a52b20e97a177b20d2
kaviyarsan2004 <kavi580580@gmail.com>
2024-05-22 15:51:00 -0400

    added some county data

---
+++ Jefferson_County/Jefferson_County_Parcels_Extract.py
@@ -0,0 +1,49 @@
+import requests
+import csv
+
+url = "https://spatialags.vhb.com/arcgis/rest/services/29823_Jefferson/Layers/MapServer/19/query"
+params = {
+    "f": "json",
+    "where": "1=1",
+    "outFields": "*",
+    "returnGeometry": "false",
+    "resultOffset": 0,
+    "resultRecordCount": 2000  
+}
+count =0
+all_features = []
+
+while True:
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        count += 2000
+        print(count)
+        features = data.get('features', [])
+
+        if not features:
+            break
+        
+        all_features.extend(features)
+        
+        params["resultOffset"] += params["resultRecordCount"]
+    else:
+        print(f"Error: {response.status_code}")
+        break
+
+if all_features:
+    field_names = list(all_features[0]['attributes'].keys())
+
+    with open('Jefferson_County_Parcels.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        
+        writer.writerow(field_names)
+        
+        for feature in all_features:
+            row = [feature['attributes'].get(field, "") for field in field_names]
+            writer.writerow(row)
+    
+    print("Data has been written to Orleans Jefferson_County_parcel.csv")
+else:
+    print("No data retrieved")
